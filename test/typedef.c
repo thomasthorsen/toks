4675059ce1506c9105c517b079c75a11abb8b01b
Thomas Thorsen <ttdkgit@gmail.com>
2013-08-22 23:04:18 +0200

    Add some references to the typedef test

--- test/typedef.c
+++ test/typedef.c
@@ -24,3 +24,29 @@ typedef void (*typee)(void);
 typedef void (typef)(void);
 
 typedef void typeg(void);
+
+void function(
+    typea a,
+    uniona uaa,
+    union unionb ub,
+    unionb ubb,
+    struct structa sa,
+    structa saa,
+    enuma ea,
+    enum enumb eaa,
+    typee e,
+    typef f,
+    typeg g)
+{
+    typea a1;
+    uniona uaa1;
+    union unionb ub1;
+    unionb ubb1;
+    struct structa sa1;
+    structa saa1;
+    enuma ea1,
+    enum enumb eaa1,
+    typee e1;
+    typef f1;
+    typeg g1;
+}
