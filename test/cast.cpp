4a8654f6975d2aa8b60cd847318201fd84139479
Thomas Thorsen <ttdkgit@gmail.com>
2013-09-03 22:26:27 +0200

    Add cast test

---
+++ test/cast.cpp
@@ -0,0 +1,18 @@
+
+int x = (int) 5;
+
+INT x = (INT) 5;
+
+UINT y = (UINT) 5;
+
+z = (my_other_type) function((MyType) x);
+
+pb = dynamic_cast<CBase*>(&d);
+
+CDerived *b = static_cast<CDerived*>(a);
+
+INT i = static_cast<INT>(d);
+
+B *b = reinterpret_cast<B*>(a);
+
+print(const_cast<CHAR*>(ch));
